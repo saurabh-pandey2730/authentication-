520b6e2e2721548735b65f1a1838ea2ad95aa926
Saurabh-Pandey <saurabh.k.pandey2018@gmail.com>
2024-01-22 18:58:16 +0530

    Login completed

--- client/src/Signup.jsx
+++ client/src/Signup.jsx
@@ -1,15 +1,19 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
  import axios from 'axios'
+ import { useNavigate } from 'react-router-dom'
 function Signup() {
  const [name, setName]=useState()
  const [email, setEmail]=useState()
  const [password, setPassword]=useState()
+ const navigate =useNavigate()
 
 const handleSubmit=(e)=>{
       e.preventDefault()
        axios.post('http://localhost:3001/register',{name,email,password})
-       .then(result=>  console.log(result))
+       .then(result=> { console.log(result)  
+    navigate('/login')
+})
        .catch(err => console.log(err))
 
 }
--- server/index.js
+++ server/index.js
@@ -8,6 +8,8 @@ app.use(cors())
 
 mongoose.connect('mongodb+srv://saurabh:shaurya2@cluster0.kcddein.mongodb.net/?retryWrites=true&w=majority')
 .then(console.log("connected"))
+
+
 app.post('/register', (req, res)=>{
 EmployeeModel.create(req.body)
 .then(employee => res.json(employee))
@@ -15,6 +17,23 @@ EmployeeModel.create(req.body)
 
 })
 
+app.post('/login', (req, res)=>{
+
+const {email, password}=req.body
+EmployeeModel.findOne({email})
+.then(user=>{
+    if(user){
+        if(user.password===password){
+            res.json("success")
+        }else{
+            res.json("The password is incorrect")
+        }
+    }else{
+         res.json("No Record existed")   
+    }
+})
+
+})
 app.listen(3001,()=>{
     console.log("server is running")
 })
