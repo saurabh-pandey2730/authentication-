8e3426dd7cfc3ef6e836e4a2e8f9f1ea8b16778e
Saurabh-Pandey <saurabh.k.pandey2018@gmail.com>
2024-01-22 18:38:59 +0530

    schema created

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const mongoose =require('mongoose')
 const cors = require("cors")
-
+const EmployeeModel = require('./models/Employee')
 const app =express()
 app.use(express.json())
 app.use(cors())
@@ -9,6 +9,9 @@ app.use(cors())
 mongoose.connect('mongodb+srv://saurabh:shaurya2@cluster0.kcddein.mongodb.net/?retryWrites=true&w=majority')
 .then(console.log("connected"))
 app.post('/register', (req, res)=>{
+EmployeeModel.create(req.body)
+.then(employee => res.json(employee))
+.catch(err => res.json(err)) 
 
 })
 
